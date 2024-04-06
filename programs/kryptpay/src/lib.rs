af118ba180b9794688d4bf7cc60fb19ed21a9d6b
gconnect <agatevureglory@gmail.com>
2024-04-06 19:00:35 +0100

    implemented transfer

--- programs/kryptpay/src/lib.rs
+++ programs/kryptpay/src/lib.rs
@@ -1,15 +1,161 @@
-use anchor_lang::prelude::*;
+use {
+    anchor_lang::prelude::*,
+    anchor_spl::{
+        associated_token::AssociatedToken,
+        token::{transfer, Mint, Token, TokenAccount, Transfer as SplTransfer}
+    },
+    solana_program::system_instruction,
+};
 
-declare_id!("7KSFNXrjkYoHbWj7YBV1kXCabY7kTnFB6VNNDAX83fbc");
+declare_id!("6k73LWhMtLhJLVyC3qQGa8pvDZ2GUXnLAb9n3juk8u3A");
 
 #[program]
 pub mod kryptpay {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn transfer_spl_tokens(ctx: Context<TransferSpl>, amount: u64) -> Result<()> {
+        // Deduct platform fee (0.05% of the lamports) before transferring
+        let platform_fee = amount * 5 / 10000;
+        let amount_after_fee = amount - platform_fee;
+
+        let destination = &ctx.accounts.to_ata;
+        let source = &ctx.accounts.from_ata;
+        let token_program = &ctx.accounts.token_program;
+        let authority = &ctx.accounts.from;
+
+        // Transfer tokens from taker to initializer
+        let cpi_accounts = SplTransfer {
+            from: source.to_account_info().clone(),
+            to: destination.to_account_info().clone(),
+            authority: authority.to_account_info().clone(),
+        };
+        let cpi_program = token_program.to_account_info();
+        
+        transfer(
+            CpiContext::new(cpi_program, cpi_accounts),
+            amount_after_fee)?;
+
+        // Transfer platform fee to the platform account
+        let platform_account = ctx.accounts.platform_account.to_account_info().clone();
+        transfer(
+            CpiContext::new(ctx.accounts.token_program.to_account_info(), SplTransfer {
+                from: ctx.accounts.from_ata.to_account_info().clone(),
+                to: platform_account,
+                authority: ctx.accounts.from.to_account_info().clone(),
+            }),
+            platform_fee,
+        )?;
+
+        // Reward user with 10 Krystal tokens
+        ctx.accounts
+            .krystal_token
+            .deposit(ctx.accounts.from, 10)?;
+
+        // mint_to(
+        //     CpiContext::new_with_signer(
+        //         ctx.accounts.token_program.to_account_info(),
+        //         MintTo { 
+        //             authority: ctx.accounts.mint.to_account_info(), 
+        //             to: ctx.accounts.token_account.to_account_info(),
+        //             krystal_token: ctx.accounts.krystal_token.to_account_info()
+        //         },
+        //         &[&[
+        //             "mint".as_bytes(),
+        //             // &[*ctx.bumps.get("mint").unwrap()]
+        //         ]]
+        //     ),
+        //     10*10^6
+        // )?;
+
+        msg!("Minted tokens");
+
         Ok(())
     }
+
+    pub fn transfer_lamports(ctx: Context<TransferLamports>, amount: u64) -> Result<()> {
+        // Calculate the platform fee (0.05% of the transaction amount)
+        let platform_fee = amount * 5 / 10000;
+
+        // Calculate the amount to transfer after deducting the platform fee
+        let amount_after_fee = amount - platform_fee;
+
+        let from_account = &ctx.accounts.from;
+        let to_account = &ctx.accounts.to;
+
+        // Create the transfer instruction
+        let transfer_instruction = system_instruction::transfer(
+            from_account.key, 
+            to_account.key, 
+            amount_after_fee
+        );
+
+        // Invoke the transfer instruction
+        anchor_lang::solana_program::program::invoke_signed(
+            &transfer_instruction,
+            &[
+                from_account.to_account_info(),
+                to_account.clone(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[],
+        )?;
+
+        // Transfer platform fee to the platform account
+        let platform_account = ctx.accounts.platform_account.to_account_info().clone();
+        let transfer_instruction = system_instruction::transfer(
+            ctx.accounts.from.key,
+            platform_account.key,
+            platform_fee,
+        );
+        anchor_lang::solana_program::program::invoke_signed(
+            &transfer_instruction,
+            &[
+                ctx.accounts.from.to_account_info(),
+                platform_account,
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[],
+        )?;
+
+        // Reward user with 10 Krystal tokens
+        // ctx.accounts
+        //     .krystal_token
+        //     .deposit(ctx.accounts.from, 10)?;
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct TransferLamports<'info> {
+    #[account(mut)]
+    pub from: Signer<'info>,
+
+    #[account(mut)]
+    /// CHECK: The `to` field is marked as mutable because it represents the account
+    ///         to which lamports are being transferred. Since the transfer operation
+    ///         requires modifying the account data, it is necessary for this field
+    ///         to be mutable. No additional checks through types are necessary for
+    ///         this field as the safety of the operation is ensured by the program logic.
+    pub to: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    // #[account(signer)]
+    // pub platform_account: AccountInfo<'info>,
+    // pub krystal_token: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+#[instruction(amount: u64)]
+pub struct TransferSpl<'info> {
+    pub from: Signer<'info>,
+    #[account(mut)]
+    pub from_ata: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub to_ata: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+    pub krystal_token: Program<'info, Token>,
+    pub platform_account: Account<'info, TokenAccount>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>
+}
+
--- tests/kryptpay.ts
+++ tests/kryptpay.ts
@@ -1,16 +1,137 @@
+// import * as anchor from "@project-serum/anchor";
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { Program } from "@project-serum/anchor"
+import {
+  Keypair,
+  PublicKey,
+  Connection,
+  clusterApiUrl
+} from "@solana/web3.js";
+import { expect } from "chai";
+import {
+  createMint,
+  mintTo,
+  createAssociatedTokenAccount,
+  getAssociatedTokenAddress,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
 import { Kryptpay } from "../target/types/kryptpay";
+import dotenv from 'dotenv'
+dotenv.config()
 
-describe("kryptpay", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const connection = new Connection(
+  clusterApiUrl('devnet'),
+  'confirmed'
+);
 
-  const program = anchor.workspace.Kryptpay as Program<Kryptpay>;
+describe("Transfer Tokens", async () => {
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const program = anchor.workspace.Kryptpay as Program<Kryptpay>
+
+  const senderPublicKey = provider.wallet.publicKey
+  
+  // const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
+  //   [Buffer.from("mint")],
+  //   program.programId
+  // )
+
+  // create keypairs
+  const keyPairString: string | undefined = process.env.KEY_PAIR;
+  let senderKeyPair: anchor.web3.Keypair | anchor.web3.Signer;
+  if (keyPairString) {
+      const keyPairBytes: number[] = keyPairString.split(",").map((value) => parseInt(value, 10));
+      const seed: Uint8Array = new Uint8Array(keyPairBytes.slice(0, 32));
+      senderKeyPair = Keypair.fromSeed(seed);
+    } else {
+      console.error("Environment variable KEY_PAIR is not set.");
+  } 
+
+  console.log("testing..")
+
+  it("It should transfer lamport", async () => {
+      // Generate keypair for the new account
+    const newAccountKp = new Keypair();
+    // Send transaction
+    const data = new anchor.BN(1000000);
+    const txHash = await program.methods
+      .transferLamports(data)
+      .accounts({
+        from: senderPublicKey,
+        to: newAccountKp.publicKey,
+      })
+      .signers([senderKeyPair])
+      .rpc();
+    console.log(`https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+
+    const newAccountBalance = await program.provider.connection.getBalance(
+      newAccountKp.publicKey
+    );
+    console.log(newAccountBalance)
+    expect(newAccountBalance === data.toNumber())
+
+  })
+
+  it("transferSplTokens", async () => {
+    // Generate keypairs for the new accounts
+    const fromKp = senderKeyPair;
+    const toKp = new Keypair();
+
+    // Create a new mint and initialize it
+    const mintKp = new Keypair();
+    const mint = await createMint(
+      provider.connection,
+      senderKeyPair,
+      fromKp.publicKey,
+      null,
+      0
+    );
+
+    // const tokenAccount = await getAssociatedTokenAddress(
+    //   mint,
+    //   provider.wallet.publicKey
+    // )
+
+    // Create associated token accounts for the new accounts
+    const fromAta = await createAssociatedTokenAccount(
+      connection,
+      senderKeyPair,
+      mint,
+      fromKp.publicKey
+    );
+    const toAta = await createAssociatedTokenAccount(
+      connection,
+      senderKeyPair,
+      mint,
+      toKp.publicKey
+    );
+    // Mint tokens to the 'from' associated token account
+    const mintAmount = 1000;
+    await mintTo(
+      connection,
+      senderKeyPair,
+      mint,
+      fromAta,
+      senderPublicKey,
+      mintAmount
+    );
+
+    // Send transaction
+    const transferAmount = new anchor.BN(500);
+    const txHash = await program.methods
+      .transferSplTokens(transferAmount)
+      .accounts({
+        from: fromKp.publicKey,
+        fromAta: fromAta,
+        toAta: toAta,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .signers([senderKeyPair, fromKp])
+      .rpc();
+    console.log(`https://explorer.solana.com/tx/${txHash}?cluster=devnet`);
+    const toTokenAccount = await connection.getTokenAccountBalance(toAta);
+    expect(toTokenAccount.value.uiAmount === transferAmount.toNumber())
+
   });
 });
